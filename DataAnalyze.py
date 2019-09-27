eef154b9a4dc1fc31015f78be42c113cf912e926
ljx <ljx@qq.com>
2019-09-27 09:12:02 +0800

    V1.02 在界面上增加按值筛选和按横坐标筛选两种方式定位原数据中的行数的功能 直接分析显示key:value中的value和key:value1:value2:value3中的value3

--- DataAnalyze.py
+++ DataAnalyze.py
@@ -37,6 +37,8 @@ class MyWindow(QMainWindow, Ui_DataAnalyze, QFileDialog):
         self.comboBox_Type.addItem("")
         self.comboBox_Type.addItem("int")
         self.comboBox_Type.addItem("float")
+        self.lineEdit_value.setText("0")
+        self.lineEdit_value2.setText("0")
         self.plot = PlotCanvas(self, width=5, height=4)
         self.verticalLayout.addWidget(self.plot)
         self.mpl_ntb = NavigationToolbar(self.plot, self)
@@ -59,6 +61,8 @@ class MyWindow(QMainWindow, Ui_DataAnalyze, QFileDialog):
         self.content=[]
         self.key_list=[]
         self.plot.clear()
+        self.textEdit.clear()
+        self.textEdit2.clear()
         key = self.lineEdit_Key.text() #获取数据名称
         file_path = self.lineEdit_FileDir.text()
         if os.path.exists(file_path):       #检查文件是否存在，若存在则打开读取其中的数据
@@ -71,33 +75,40 @@ class MyWindow(QMainWindow, Ui_DataAnalyze, QFileDialog):
             self.progressBar.setRange(0, range_number) #根据文件行数修改range范围
             count = 0
             for line in file_open:
-                new = line.split(':') #非CSV文件 直接按符号分割
-                if(len(new) > 3):    #key:value1:value2:value3类型
-                # if(len(new) == 2): #key:value1类型
-                    value_index = 3  #取值value3
-                    name = new[0].strip().replace("\n","") #去掉keyName前后空格和换行
-                    value = new[value_index].strip().replace("\n","") 
-                    value = new[value_index].strip().replace("\"","") #去掉value的换行、引号等字符
-                    if (is_number(value)): #判断值是否为数字
-                        self.key_list.append(name) #记录所有的KeyName
-                        if name == key:  #数据名称
-                            if(type(eval(value)) == int):
-                                number = int(value) #整型数据
-                                self.comboBox_Type.setCurrentIndex(1)
-                            elif(type(eval(value)) == float):
-                                number = float(value) #浮点型数据
-                                self.comboBox_Type.setCurrentIndex(2)
-                            else:
-                                self.comboBox_Type.setCurrentIndex(0)
-                                print("data type erro")
-                            # if number >= 10: #按值筛选
-                                # self.textEdit.insertPlainText('%d'%(len(self.content)) +'\r\n') #输出数据所在横坐标
-                            self.content.append(number)
-                            # self.textEdit.insertPlainText(number +'\r\n')
                 count = count + 1
-                # if (len(self.content) >= 5470): #按横坐标筛选
-                    # self.textEdit.insertPlainText('%d'%(count) +'\r\n')  #输出原始数据所在行数
                 self.progressBar.setValue(count)
+                new = line.split(':') #非CSV文件 直接按符号分割
+                info_count = len(new)
+                if(info_count == 4):    #key:value1:value2:value3类型
+                    value_index = 3  #取值value3
+                elif(info_count == 2): #key:value1类型
+                    value_index = 1  #取值value3
+                else:
+                    continue
+                name = new[0].strip().replace("\n","") #去掉keyName前后空格和换行
+                value = new[value_index].strip().replace("\n","") 
+                value = new[value_index].strip().replace("\"","") #去掉value的换行、引号等字符
+                if (is_number(value)): #判断值是否为数字
+                    self.key_list.append(name) #记录所有的KeyName
+                    if name == key:  #数据名称
+                        if(type(eval(value)) == int):
+                            number = int(value) #整型数据
+                            self.comboBox_Type.setCurrentIndex(1)
+                        elif(type(eval(value)) == float):
+                            number = float(value) #浮点型数据
+                            self.comboBox_Type.setCurrentIndex(2)
+                        else:
+                            self.comboBox_Type.setCurrentIndex(0)
+                            print("data type erro")
+                        editvalue = int(self.lineEdit_value.text())
+                        if (number >= editvalue) and (editvalue > 0): #按值筛选
+                            # self.textEdit.insertPlainText('%d'%(len(self.content)) +'\r\n') #输出数据所在横坐标
+                            self.textEdit.insertPlainText('%d'%(count) +'\r\n')  #输出原始数据所在行数
+                        self.content.append(number)
+                        # self.textEdit.insertPlainText(number +'\r\n')
+                editvalue2 = int(self.lineEdit_value2.text())
+                if (len(self.content) >= editvalue2) and (editvalue2 > 0): #按横坐标筛选
+                    self.textEdit2.insertPlainText('%d'%(count) +'\r\n')  #输出原始数据所在行数
             self.key_list = list(set(self.key_list))    #去掉KeyName中所有重复项
             self.key_list.sort()                        #排序KeyName
             self.listWidget_KeyName.clear()
